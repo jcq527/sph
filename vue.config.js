f4dfa7deae30aca7fb0f943cea1e2a0a02f66246
jcq527 <1667535714@qq.com>
2022-07-19 22:04:36 +0800

    SPH_0719

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = ({
+    //关闭eslint
+    lintOnSave: false,
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://gmall-h5-api.atguigu.cn'
+            }
+        }
+    }
+
+})
